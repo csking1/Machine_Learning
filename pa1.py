e8e07ebb5c0f5c05b1e61c660408c4d6f31dc6ff
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-04-01 18:07:26 -0500

    not many new changes

--- pa1.py
+++ pa1.py
@@ -1,12 +1,26 @@
 import csv
+import pandas as pd
+from genderize import Genderize
+
 
 def parse_data(filename):
+    no_gender = []
+    # print (Genderize().get(['James', 'Eva']))
 
-    with open(filename, 'rU') as f:
-        fields = csv.reader(f)
-        attrs = next(fields)
-        for row in fields:
-            print (row)
+    data_df = pd.read_csv(filename)
+    print (data_df.columns)  #gives headers
+    # print (data_df.describe())
+    # print (data_df.std())
+    if not data_df["Gender"]:
+        l = data_df['First_name'].tolist()
+        print (l)
+
+
+    # with open(filename, 'rU') as f:
+    #     fields = csv.reader(f)
+    #     attrs = next(fields)
+    #     for row in fields:
+    #         print (row)
 
 
 
