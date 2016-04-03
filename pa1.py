0c74d8742eefb0fbe26b6732baf292a4b5b10e37
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-04-02 19:24:01 -0500

    histogram almost complete

--- pa1.py
+++ pa1.py
@@ -1,29 +1,40 @@
 import csv
 import pandas as pd
 from genderize import Genderize
+import matplotlib.pyplot as plt
 
 
-def parse_data(filename):
-    no_gender = []
-    # print (Genderize().get(['James', 'Eva']))
-
+def parse_data(filename, file1):
+ 
     data_df = pd.read_csv(filename)
-    print (data_df.columns)  #gives headers
+    # print ("This is the number of entries for each column")
+    # print (data_df.count())  #gives headers
+    print ("-----------------------------------------")
+    new_df = data_df.fillna(data_df.mean())
+    gender = new_df[new_df['Gender'].notnull()]
+    no_gender = new_df[new_df['Gender'].isnull()]
+    # for index, row in no_gender.iterrows():
+    #     name = row["First_name"].split(" ")
+    #     result =  Genderize().get(name)
+    #     no_gender.is_copy = False  
+    #     no_gender.loc[index, "Gender"] = result[0]["gender"]
+    merged = no_gender.append(gender, ignore_index = True)
+    print (merged.describe())
+    print (merged.hist())
+    merged.to_csv(file1, sep='\t')
+    fig = plt.hist()
+    merged.plot.hist()
+    savefig('fig1.png')
+
     # print (data_df.describe())
     # print (data_df.std())
-    if not data_df["Gender"]:
-        l = data_df['First_name'].tolist()
-        print (l)
 
-
-    # with open(filename, 'rU') as f:
-    #     fields = csv.reader(f)
-    #     attrs = next(fields)
-    #     for row in fields:
-    #         print (row)
 
 
 
 
 raw_filename = "mock_student_data.csv"
-parse_data(raw_filename)
+file1 = "filled_out.csv"
+file2 = "conditional.csv"
+file3 = "dropped.csv"
+parse_data(raw_filename, file1)
