8bd13398c3975a824097f02f74e07e0ab2620194
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-04-03 20:14:43 -0500

    Changes

--- pa1.py
+++ pa1.py
@@ -1,4 +1,4 @@
-import csv
+
 import pandas as pd
 from genderize import Genderize
 import matplotlib.pyplot as plt
@@ -19,12 +19,24 @@ def parse_data(filename, file1):
     #     no_gender.is_copy = False  
     #     no_gender.loc[index, "Gender"] = result[0]["gender"]
     merged = no_gender.append(gender, ignore_index = True)
-    print (merged.describe())
-    print (merged.hist())
+    # print (merged.describe())
+    merged.hist()
+    plt.savefig('histogram.png')
+    plot = merged.plot.hist()
+    fig = plot.get_figure()
+    fig.savefig("plot1.png")
     merged.to_csv(file1, sep='\t')
-    fig = plt.hist()
-    merged.plot.hist()
-    savefig('fig1.png')
+    # fig, axes = plt.subplots(nrows=3, ncols=1)
+    # for i, c in enumerate(merged.columns):
+    #     merged[c].plot(kind='bar', ax=axes[i], figsize=(12, 10), title=c)
+    # plt.savefig('Plot1.png', bbox_inches='tight')
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1, 1, 1)
+    # ax.hist(sample, color='y', alpha=0.5)
+
+    # merged.plot.hist()
+    # savefig('fig1.png')
 
     # print (data_df.describe())
     # print (data_df.std())
