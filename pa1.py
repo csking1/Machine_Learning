56521c56191f37d3a3e4eb8c48a94f6cd767f11b
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-04-04 12:30:25 -0500

    changes

--- pa1.py
+++ pa1.py
@@ -4,49 +4,56 @@ from genderize import Genderize
 import matplotlib.pyplot as plt
 
 
-def parse_data(filename, file1):
+def parse_data(filename, file1, file2):
  
     data_df = pd.read_csv(filename)
-    # print ("This is the number of entries for each column")
-    # print (data_df.count())  #gives headers
-    print ("-----------------------------------------")
-    new_df = data_df.fillna(data_df.mean())
-    gender = new_df[new_df['Gender'].notnull()]
-    no_gender = new_df[new_df['Gender'].isnull()]
+    print ("------------------initial statstics--------------")
+    print (data_df.describe())
+    print (data_df.median())
+    print (data_df.mode())
+    # new_df = data_df.fillna(data_df.mean())
+    gender = data_df[data_df['Gender'].notnull()]
+    no_gender = data_df[data_df['Gender'].isnull()]
     # for index, row in no_gender.iterrows():
     #     name = row["First_name"].split(" ")
     #     result =  Genderize().get(name)
     #     no_gender.is_copy = False  
     #     no_gender.loc[index, "Gender"] = result[0]["gender"]
     merged = no_gender.append(gender, ignore_index = True)
-    # print (merged.describe())
-    merged.hist()
-    plt.savefig('histogram.png')
-    plot = merged.plot.hist()
-    fig = plot.get_figure()
-    fig.savefig("plot1.png")
-    merged.to_csv(file1, sep='\t')
-    # fig, axes = plt.subplots(nrows=3, ncols=1)
-    # for i, c in enumerate(merged.columns):
-    #     merged[c].plot(kind='bar', ax=axes[i], figsize=(12, 10), title=c)
-    # plt.savefig('Plot1.png', bbox_inches='tight')
+    conditional(merged, "uncond_hist.png", file1, conditional=False )
+    conditional(merged,"condit_hist.png", file2, conditional=True)
 
-    # fig = plt.figure()
-    # ax = fig.add_subplot(1, 1, 1)
-    # ax.hist(sample, color='y', alpha=0.5)
+def conditional(dataframe, png_name, file, conditional):
+    if not conditional:
+        new_dataframe = dataframe.fillna(dataframe.mean())
+        # new_dataframe.to_csv(file, sep='\t')
+        # graph_histograms(new_dataframe, png_name)
+    else:
+        # print (dataframe.count())
+        print ("--------in unconditional function-------------")
+        graduated = dataframe[dataframe.Graduated == "Yes"]
+        not_grad = dataframe[dataframe.Graduated == "No"]
+        print ("number of entries for non-grads", not_grad.count())
+        print ("number of entries for grad", graduated.count())
+        graduated1 = graduated.fillna(graduated.mean())
+        total = graduated1.append(not_grad, ignore_index=True)
+        total.to_csv(file, sep='\t')
+        graph_histograms(total, png_name)
 
-    # merged.plot.hist()
-    # savefig('fig1.png')
+def graph_histograms(dataframe, png_name):
 
-    # print (data_df.describe())
-    # print (data_df.std())
+    dataframe.hist()
+    plt.savefig(png_name)
 
-
+    print (dataframe.count())
+    print ("----------------summary statistics-------")
+    print ("mode", dataframe.mode())
+    print ("median", dataframe.median())
 
 
 
 raw_filename = "mock_student_data.csv"
-file1 = "filled_out.csv"
-file2 = "conditional.csv"
+file1 = "uncondit.csv"
+file2 = "condit.csv"
 file3 = "dropped.csv"
-parse_data(raw_filename, file1)
+parse_data(raw_filename, file1, file2)
