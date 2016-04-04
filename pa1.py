6da9adb3728694b75b15d71c3d949aab13a51431
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-04-04 13:20:56 -0500

    Final Submission for PA1

--- pa1.py
+++ pa1.py
@@ -1,3 +1,8 @@
+#Machine Learning for Public Policy: HW1
+#Charity King
+#April 4, 2016
+#csking1@uchicago.edu
+
 
 import pandas as pd
 from genderize import Genderize
@@ -5,32 +10,46 @@ import matplotlib.pyplot as plt
 
 
 def parse_data(filename, file1, file2):
+    '''
+    Takes raw filename and 2 CSV filenames
+    Calls conditional function to create two csv files
+    '''
  
     data_df = pd.read_csv(filename)
-    print ("------------------initial statstics--------------")
-    print (data_df.describe())
-    print (data_df.median())
-    print (data_df.mode())
-    # new_df = data_df.fillna(data_df.mean())
+    print_summary_stats(data_df)
     gender = data_df[data_df['Gender'].notnull()]
     no_gender = data_df[data_df['Gender'].isnull()]
-    # for index, row in no_gender.iterrows():
-    #     name = row["First_name"].split(" ")
-    #     result =  Genderize().get(name)
-    #     no_gender.is_copy = False  
-    #     no_gender.loc[index, "Gender"] = result[0]["gender"]
+    for index, row in no_gender.iterrows():
+        name = row["First_name"].split(" ")
+        result =  Genderize().get(name)
+        no_gender.is_copy = False  
+        no_gender.loc[index, "Gender"] = result[0]["gender"]
     merged = no_gender.append(gender, ignore_index = True)
-    conditional(merged, "uncond_hist.png", file1, conditional=False )
+    conditional(merged, "uncond_hist.png", file1, conditional=False)
     conditional(merged,"condit_hist.png", file2, conditional=True)
 
+def print_summary_stats(dataframe):
+    '''
+    Takes pandas dataframe and prints mean, median, mode, sd for each
+    column
+    '''
+    print ("------------Summary Statistics-------------")
+    print (dataframe.describe())
+    print ("Median:", dataframe.median())
+    print ("Mode:", dataframe.mode())
+
 def conditional(dataframe, png_name, file, conditional):
+    '''
+    Takes dataframe, png name, csv filename, and conditional switch
+    Creates a csv file after filling missing data conditional or unconditional
+    Calls histogram graphing function
+    '''
     if not conditional:
         new_dataframe = dataframe.fillna(dataframe.mean())
-        # new_dataframe.to_csv(file, sep='\t')
-        # graph_histograms(new_dataframe, png_name)
+        new_dataframe.to_csv(file, sep='\t')
+        graph_histograms(new_dataframe, png_name)
     else:
-        # print (dataframe.count())
-        print ("--------in unconditional function-------------")
+        print ("--------in conditional function-------------")
         graduated = dataframe[dataframe.Graduated == "Yes"]
         not_grad = dataframe[dataframe.Graduated == "No"]
         print ("number of entries for non-grads", not_grad.count())
@@ -41,14 +60,13 @@ def conditional(dataframe, png_name, file, conditional):
         graph_histograms(total, png_name)
 
 def graph_histograms(dataframe, png_name):
+    '''
+    Takes dataframe and png_name and creates histogram png for dataset
+    '''
 
     dataframe.hist()
     plt.savefig(png_name)
-
-    print (dataframe.count())
-    print ("----------------summary statistics-------")
-    print ("mode", dataframe.mode())
-    print ("median", dataframe.median())
+    print_summary_stats(dataframe)
 
 
 
