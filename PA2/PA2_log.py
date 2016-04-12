2cb19e54885b3090db77700e88ccd8f48b556ed3
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-04-11 23:14:37 -0500

    checkin

--- PA2/PA2_log.py
+++ PA2/PA2_log.py
@@ -8,6 +8,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 TARGET = "Dlqin2yrs"
@@ -15,15 +16,21 @@ RENAME = ["Index", "Dlqin2yrs", "Unsecured", "Age", \
      "30to59days", "Debtratio", "Monthlyincome",  "Opencredit", \
     "90days","realestate", "60to89days", "Dependents"]
 
-def ML_Pipeline(training, testing):
+def ML_Pipeline(training, testing, file1, file2, png1, png2):
     '''
     Wrapper function that takes training/
     testing data
     '''
-    train = explore_data(training)
-    filled = process_data(train)
-    build_model(filled)
-
+    print ("testing data stats")
+    train = explore_data(training, png1)
+    print ("unlabeled data stats")
+    unlab = explore_data(testing, png2)
+    filled = process_data(train, file1)
+    filled_unlabeled = process_data(unlab, file2)
+    train, test = train_test_split(filled, test_size = .1)
+    model = build_model(train)
+    test_model(test, model)
+    classify_data(filled_unlabeled, model)
 def build_model(dataframe):
 
     y = dataframe[TARGET]
@@ -31,43 +38,41 @@ def build_model(dataframe):
     x = dataframe.drop(TARGET, 1)
     model = LogisticRegression()
     model= model.fit(x, y)
-    print (model.score(x, y))
+    print  ("Training Set with accuracy rating of:", model.score(x, y))
+    return model
+def test_model (dataframe, model): 
+    y = dataframe[TARGET]
+    y = np.ravel(y)
+    x = dataframe.drop(TARGET, 1)
+    testing = model.score(x, y)
+    print ("Testing had a {} Accuracy Score". format(testing))
+def classify_data(dataframe, model):
 
+    y = dataframe[TARGET]
+    y = np.ravel(y)
+    x = dataframe.drop(TARGET, 1)
+    predictions = model.predict(x)
+    print (predictions)
 
-
-
-
-
-
-def explore_data(training):
+def explore_data(dataframe, pngfile):
     '''
     Takes raw filename and 2 CSV filenames
     Calls conditional function to create two csv files
     '''
  
-    train = pd.read_csv(training)
-    train.columns = RENAME
-    # print (train["Dlqin2yrs"])
-    print_summary_stats(train)
-    graph_histograms(train, "initial.png")
-    return train
-    # gender = data_df[data_df['Gender'].notnull()]
-    # no_gender = data_df[data_df['Gender'].isnull()]
-    # for index, row in no_gender.iterrows():
-    #     name = row["First_name"].split(" ")
-    #     result =  Genderize().get(name)
-    #     no_gender.is_copy = False  
-    #     no_gender.loc[index, "Gender"] = result[0]["gender"]
-    # merged = no_gender.append(gender, ignore_index = True)
-    # conditional(merged, "uncond_hist.png", file1, conditional=False)
-    # conditional(merged,"condit_hist.png", file2, conditional=True)
+    df = pd.read_csv(dataframe)
+    df.columns = RENAME
+    df = df.drop("Index", 1)
+    print_summary_stats(df)
+    graph_histograms(df, pngfile)
+    return df
 
 def print_summary_stats(dataframe):
     '''
     Takes pandas dataframe and prints mean, median, mode, sd for each
     column
     '''
-    pass
+    
     # print (dataframe.count())
     # print (dataframe.describe())
     # print ("Median:", dataframe.median())
@@ -75,12 +80,16 @@ def print_summary_stats(dataframe):
     # print (dataframe.groupby("Dlqin2yrs").size())    
     # print (dataframe.groupby("Dlqin2yrs").mean())
 
-
-def process_data(dataframe):
-    # print ("preposcessing data-----------------")
-    filled = dataframe.fillna(dataframe.median())
-    #potentially categorize
-    return filled
+    pass
+def process_data(dataframe, filename):
+    dataframe["Dependents"]= dataframe["Dependents"].fillna(0)
+    filled_df = dataframe.fillna(dataframe.median())
+    filename = filled_df.to_csv(filename, sep='\t')
+    return filled_df
+def create_bins():
+    pass
+def cat_to_binary():
+    pass
     
 
 
@@ -93,7 +102,7 @@ def graph_histograms(dataframe, png_name):
     "90days","realestate", "60to89days", "Dependents"]
     '''
 
-    dataframe.Dlqin2yrs.hist(xlabelsize=2)
+    dataframe.Dlqin2yrs.hist(bins=4)
     plt.title("Histogram of Delinquency Risk")
     plt.xlabel('Dlqin Risk within 2 Years')
     plt.ylabel("Frequency")
@@ -103,7 +112,9 @@ def graph_histograms(dataframe, png_name):
 
 training = "cs-training.csv"
 testing = "cs-test.csv"
-file1 = "uncondit.csv"
-file2 = "condit.csv"
+file1 = "filledtraining.csv"
+file2 = "filledtesting.csv"
+png1 = "training.png"
+png2 = "testing.png"
 
-ML_Pipeline(training,testing)
+ML_Pipeline(training,testing, file1, file2, png1, png2)
