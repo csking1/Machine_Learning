2f302106040133b277fdc6ac639120f15acc9261
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-04-12 22:52:47 -0500

    Changes to PA2

--- PA2/PA2_log.py
+++ PA2/PA2_log.py
@@ -16,7 +16,21 @@ RENAME = ["Index", "Dlqin2yrs", "Unsecured", "Age", \
      "30to59days", "Debtratio", "Monthlyincome",  "Opencredit", \
     "90days","realestate", "60to89days", "Dependents"]
 
-def ML_Pipeline(training, testing, file1, file2, png1, png2):
+
+def model_switch(training, testing, file1, file2, file3, png1, png2):
+    print ("First model using Continuous Age variable")
+    ML_Pipeline(training, testing, file1, file2, file3, png1, png2, discretized=False)
+    print ("Second Model using Discretized Age Variable")
+ 
+    file1 = "dis_filledtraining.csv"
+    file2 = "dis_filledtesting.csv"
+    file3 = "dis_classifiedtesting.csv"
+    png1 = "dis_training.png"
+    png2 = "dis_testing.png"
+
+    ML_Pipeline(training, testing, file1, file2, file3, png1, png2, True)
+
+def ML_Pipeline(training, testing, file1, file2, file3, png1, png2, discretized):
     '''
     Wrapper function that takes training/
     testing data
@@ -27,10 +41,14 @@ def ML_Pipeline(training, testing, file1, file2, png1, png2):
     unlab = explore_data(testing, png2)
     filled = process_data(train, file1)
     filled_unlabeled = process_data(unlab, file2)
+    if discretized:
+        filled=create_bins(filled, 'Age')
+        filled_unlabeled = create_bins(filled_unlabeled, 'Age')
     train, test = train_test_split(filled, test_size = .1)
     model = build_model(train)
     test_model(test, model)
-    classify_data(filled_unlabeled, model)
+    classify_data(filled_unlabeled, model, file3)
+
 def build_model(dataframe):
 
     y = dataframe[TARGET]
@@ -46,13 +64,18 @@ def test_model (dataframe, model):
     x = dataframe.drop(TARGET, 1)
     testing = model.score(x, y)
     print ("Testing had a {} Accuracy Score". format(testing))
-def classify_data(dataframe, model):
+def classify_data(dataframe, model, filename):
 
     y = dataframe[TARGET]
     y = np.ravel(y)
     x = dataframe.drop(TARGET, 1)
     predictions = model.predict(x)
-    print (predictions)
+    p = pd.DataFrame(predictions)
+    p.rename(columns={p.columns[0]: "Dlqin2yrs"}, inplace=True)
+    result = pd.concat([p, x], axis=1, join_axes=[p.index])
+    filename = result.to_csv(filename, sep='\t')
+    graph_histograms(result, "classified.png")
+    print (p.groupby("Dlqin2yrs").size())    
 
 def explore_data(dataframe, pngfile):
     '''
@@ -77,6 +100,7 @@ def print_summary_stats(dataframe):
     # print (dataframe.describe())
     # print ("Median:", dataframe.median())
     # print ("Mode:", dataframe.mode())
+    # print ("stats for y var.......")
     # print (dataframe.groupby("Dlqin2yrs").size())    
     # print (dataframe.groupby("Dlqin2yrs").mean())
 
@@ -86,11 +110,25 @@ def process_data(dataframe, filename):
     filled_df = dataframe.fillna(dataframe.median())
     filename = filled_df.to_csv(filename, sep='\t')
     return filled_df
-def create_bins():
-    pass
-def cat_to_binary():
-    pass
-    
+def create_bins(dataframe, variable):
+    '''
+    Takes a dataframe and the Age variable
+    Creates four discretized bins
+    '''
+    dataframe['Age20-30'] = dataframe.apply \
+    (lambda row: 1 if ((row[variable] >= 20 and row[variable] < 30)) \
+        else 0, axis=1)
+    dataframe['Age30-40'] = dataframe.apply \
+    (lambda row: 1 if ((row[variable] >= 30 and row[variable] < 40)) \
+        else 0, axis=1)
+    dataframe['Age40-60'] = dataframe.apply \
+    (lambda row: 1 if ((row[variable] >= 40 and row[variable] < 60)) \
+        else 0, axis=1)
+    dataframe['Age60+'] = dataframe.apply \
+    (lambda row: 1 if ((row[variable] >= 60 and row[variable] < 120)) \
+        else 0, axis=1)
+
+    return dataframe
 
 
 def graph_histograms(dataframe, png_name):
@@ -108,13 +146,12 @@ def graph_histograms(dataframe, png_name):
     plt.ylabel("Frequency")
     plt.savefig(png_name)
 
-
-
 training = "cs-training.csv"
 testing = "cs-test.csv"
 file1 = "filledtraining.csv"
 file2 = "filledtesting.csv"
+file3 = "classifiedtesting.csv"
 png1 = "training.png"
 png2 = "testing.png"
 
-ML_Pipeline(training,testing, file1, file2, png1, png2)
+model_switch(training,testing, file1, file2, file3, png1, png2)
