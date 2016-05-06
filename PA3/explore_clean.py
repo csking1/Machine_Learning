99f04d6796317bedc10e9dd9153d19d0e526b4ed
Charity Susan King <csking1@cs.uchicago.edu>
2016-05-06 00:00:20 -0500

    Magic Loop running...thank you baby jesus

--- PA3/explore_clean.py
+++ PA3/explore_clean.py
@@ -4,6 +4,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+   # Need to write out for each model:
+    #     -Model name
+    #     -Processing time
+    #     -Parameters
+    #     -Evaluation Metrics: Accuracy, Precision, Recall
+    #     -Plot AUC, ROC curves
+
+
+OUTLIER = "Age"  #AGE COLUMN
 TARGET = "Dlqin2yrs"
 RENAME = ["Index", "Dlqin2yrs", "Unsecured", "Age", \
      "30to59days", "Debtratio", "Monthlyincome",  "Opencredit", \
@@ -19,35 +29,44 @@ def read_data(filename):
     return df.drop(df.columns[[0]], axis = 1)
 
 def data_summary(dataframe):
-    print("----------------Percentiles:-------------" "\n", np.round(dataframe.describe(percentiles = [.5]), 2).to_string(justify = "left"))
-    print("----------------Mean:--------------------" "\n", dataframe.mean().to_string(float_format = "{:.2f}".format))
-    print("----------------Median:------------------" "\n", dataframe.median().to_string(float_format = "{:.2f}".format))
-    print("----------------Standard Deviation:------" "\n", dataframe.std().to_string(float_format = "{:.2f}".format))
-    print("----------------Mode:--------------------" '\n', dataframe.mode().to_string(index = False))
-    print("----------------Correlation Matrix:------" "\n", dataframe.corr())
-    print("----------------Missing Values:----------" "\n", dataframe.isnull().sum().to_string())
+    pass
+
+    # print("----------------Percentiles:-------------" "\n", np.round(dataframe.describe(percentiles = [.5]), 2).to_string(justify = "left"))
+    # print("----------------Mean:--------------------" "\n", dataframe.mean().to_string(float_format = "{:.2f}".format))
+    # print("----------------Median:------------------" "\n", dataframe.median().to_string(float_format = "{:.2f}".format))
+    # print("----------------Standard Deviation:------" "\n", dataframe.std().to_string(float_format = "{:.2f}".format))
+    # print("----------------Mode:--------------------" '\n', dataframe.mode().to_string(index = False))
+    # print("----------------Correlation Matrix:------" "\n", dataframe.corr())
+    # print("----------------Missing Values:----------" "\n", dataframe.isnull().sum().to_string())
 
 def graph_data(dataframe):
-    dataframe.groupby(dataframe.columns[0]).size().plot(kind = "bar", width = 1, rot = 0)
+    # dataframe.groupby(dataframe.columns[0]).size().plot(kind = "bar", width = 1, rot = 0)
+    # plt.show()
+    # for name in dataframe.columns[1:]:
+    #     dataframe.groupby(name).size().plot()
+    #     plt.show()
+    ################Specialized graphs##################
+    dataframe[dataframe.Debtratio < 3].Debtratio.hist(bins=2)
     plt.show()
-    for name in dataframe.columns[1:]:
-        dataframe.groupby(name).size().plot()
-        plt.show()
+   
 
-def impute_data(dataframe, mean=False, median=False):
+
+    
+def impute_data(dataframe, mean=False, median=False): 
     header = list(dataframe.columns)
     for each in header:
         if dataframe[each].isnull().values.any():
             if mean:
                 dataframe[each] = dataframe[each].fillna(dataframe[each].mean())
+    
             else:
                 dataframe[each] = dataframe[each].fillna(dataframe[each].median())
+
 
 
 def binary_variable(dataframe, variable):
     data[column] = data[column].apply(lambda x: 0 if \
         column == variable else 1)
-
 
 def binning_data(dataframe, variable, bins):
 
--- PA3/pipeline.py
+++ PA3/pipeline.py
@@ -3,7 +3,7 @@
 #PA3: Machine Learning Pipeline
 #May 3, 2016
 #csking1@uchicago.edu
-
+import csv
 import pandas as pd
 import explore_clean as exp
 import numpy as np
@@ -18,8 +18,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import *
 import time
 
-MODELS = ['LR','KNN','RF', 'ET', 'AB', 'SVM', 'LR','GB','NB','DT']
- 
+MODELS = ['LR','KNN']
+
+# MODELS = ['LR','KNN','RF', 'ET', 'AB', 'SVM', 'LR','GB','NB','DT']
+
 
 clfs = {'RF': RandomForestClassifier(n_estimators=50, n_jobs=-1),
         'ET': ExtraTreesClassifier(n_estimators=10, n_jobs=-1, criterion='entropy'),
@@ -45,48 +47,92 @@ grid = {
     'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],'algorithm': ['auto','ball_tree','kd_tree']}
            }
 
+def magic_loop(dataframe, x, y):
+
+    with open('models.csv', 'w', newline='') as csvfile:
+        w = csv.writer(csvfile, delimiter=',')
+        w.writerow(['MODEL', 'PARAMETERS', 'PRECISION', 'RECALL', 'AUC', 'F1', 'ACCURACY', 'Time'])
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+        best_model = ''
+        top_AUC = 0
+        best_parameters = ''
+
+        for index,clf in enumerate([clfs[x] for x in MODELS]):
+            current_model = MODELS[index]
+            print (current_model)
+            parameter_values = grid[current_model]
+
+            for p in ParameterGrid(parameter_values):
+                try:
+                    clf.set_params(**p)
+                    y_pred_probs = clf.fit(x_train, y_train).predict_proba(x_test)[:,1]
+                    plot_precision_recall_n(y_test, y_pred_probs, clf)
+                    precision, accuracy, recall, f1, threshold, AUC = model_evaluation(y_test, y_pred_probs,.05)
+                    w.writerow([current_model, p, precision, recall, AUC, f1, accuracy])
+                    if AUC > top_AUC:
+                        top_AUC = AUC
+                        best_model = current_model
+                        best_parameters = p
+                except (IndexError, e):
+                    continue
+        print ("this is the best_model", best_model)
+
+
+def model_evaluation(y_true, y_scores, k):
+
+    threshold = np.sort(y_scores)[::-1][int(k*len(y_scores))]
+    y_pred = np.asarray([1 if i >= threshold else 0 for i in y_scores])
+    precision = metrics.precision_score(y_true, y_pred)
+    accuracy = metrics.accuracy_score(y_true, y_pred)
+    recall = metrics.recall_score(y_true, y_pred)
+    f1 = metrics.f1_score(y_true, y_pred)
+    AUC = metrics.roc_auc_score(y_true, y_pred)
+    return precision, accuracy, recall, f1, threshold, AUC
+
+
+def plot_precision_recall_n(y_true, y_prob, model_name):
+    '''
+    Takes the model, plots precision and recall curves
+    '''
+
+    return 
+
+    precision_curve, recall_curve, pr_thresholds = precision_recall_curve(y_true, y_prob)
+    precision_curve = precision_curve[:-1]
+    recall_curve = recall_curve[:-1]
+    pct_above_per_thresh = []
+    number_scored = len(y_score)
+
+    for value in pr_thresholds:
+        num_above_thresh = len(y_score[y_score >= value])
+        pct_above_thresh = num_above_thresh / float(number_scored)
+        pct_above_per_thresh.append(pct_above_thresh)
+
+    pct_above_per_thresh = np.array(pct_above_per_thresh)
+    plt.clf()
+    fig, ax1 = plt.subplots()
+    ax1.plot(pct_above_per_thresh, precision_curve, 'b')
+    ax1.set_xlabel('percent of population')
+    ax1.set_ylabel('precision', color='b')
+    ax2 = ax1.twinx()
+    ax2.plot(pct_above_per_thresh, recall_curve, 'r')
+    ax2.set_ylabel('recall', color='r')
+
+    name = model_name
+    plt.title(name)
+    plt.show()
+
+
 def go(training, testing):
-	
+
     labels = exp.read_data(testing)
     train = exp.read_data(training)
-    # exp.data_summary(train)
+    exp.data_summary(train)
     # exp.graph_data(train)
     exp.impute_data(train, mean=False, median=True)
     x, y = exp.feature_generation(train)
-    main_pipeline(train, x, y)
-
-def main_pipeline(dataframe, x, y):
-
-    # Need to write out for each model:
-    #     -Model name
-    #     -Processing time
-    #     -Parameters
-    #     -Evaluation Metrics: Accuracy, Precision, Recall
-    #     -Plot AUC, ROC curves
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-    for index,clf in enumerate([clfs[x] for x in MODELS]):
-        current_model = MODELS[index]
-        print ("Training {} model".format(current_model))
-        parameter_values = grid[current_model]
-        for p in ParameterGrid(parameter_values):
-            try:
-                clf.set_params(**p)
-                y_pred_probs = clf.fit(x_train, y_train).predict_proba(x_test)[:,1]
-                print (precision_at_k(y_test,y_pred_probs,.05))
-
-            except (IndexError, e):
-                print ('Error:',e)
-                continue
-
-# def accuracy (x, y):
-#     training = model.score(x, y)
-#     print ("Testing had a {} Accuracy Score". format(testing))
-
-def precision_at_k(y_true, y_scores, k):
-    threshold = np.sort(y_scores)[::-1][int(k*len(y_scores))]
-    y_pred = np.asarray([1 if i >= threshold else 0 for i in y_scores])
-    return metrics.precision_score(y_true, y_pred)
+    magic_loop(train, x, y)
 
 if __name__ == '__main__':
 	training = "cs-training.csv"
